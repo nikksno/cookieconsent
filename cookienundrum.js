f65f17af81224544f7b48323670fb8502d8d3d07
Nicolas North <github@nikksno.com>
2015-09-18 15:32:07 +0200

    Update cookienundrum.js

--- cookienundrum.js
+++ cookienundrum.js
@@ -501,7 +501,7 @@ var cc =
             jQuery('#cc-approve-button-allsites').hide();
         }
        
-       var bannerHeight = document.getElementById('cc-notification')[0].offsetHeight;
+       var bannerHeight = jQuery('#cc-notification').height();
        var bpHeight = 58;
        
        if ($bannerHeight > $bpHeight) {
